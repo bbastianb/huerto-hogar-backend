f8c75768331efbcc2572e2da7e31a70adeffb162
BBastian <ba.burgose@duocuc.cl>
2025-11-11 13:05:16 -0300

    carpeta base

---
+++ src/main/java/com/abs/huerto_hogar/HuertoHogarApplication.java
@@ -0,0 +1,13 @@
+package com.abs.huerto_hogar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuertoHogarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuertoHogarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abs/huerto_hogar/HuertoHogarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abs.huerto_hogar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuertoHogarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
